9275fd4fa2ea5a3cd48fa367fcf79c8c55c3d296
shirishaerrabolu-ui <shirishaerrabolu@gmail.com>
2026-08-12 21:11:44 +0530

    Add files via upload

---
+++ WebTech1.py
@@ -0,0 +1,9 @@
+print("I'm in 7th grade(12 years old) , studying in JGS Mallapur,Nacharam")
+
+print("I live in Hyderabad, Telangana, India")
+
+print("I like to ride cycle and eat junk food")
+
+print("My Name is Olluri Shaurya")
+
+print("The result of 6 + 7 is:",6 + 7)
