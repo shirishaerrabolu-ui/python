95882a1cf2a7a8379bd53a8bdc6711b836ea3b43
shirishaerrabolu-ui <shirishaerrabolu@gmail.com>
2026-08-19 20:05:35 +0530

    Add files via upload

---
+++ Codesphere2.py
@@ -0,0 +1,24 @@
+print("======================================")
+print("        MY PERSONAL GOALS")
+print("======================================")
+
+name = input("Enter your name: ")
+goal = input("Enter your main goal: ")
+target_month = input("Enter your target month: ")
+reason = input("Why do you want to achieve this goal? ")
+daily_action = input("What will you do every day to achieve it? ")
+
+print()
+print("======================================")
+print("          PERSONAL GOAL DISPLAY")
+print("======================================")
+
+print("Name:", name)
+print("Main Goal:", goal)
+print("Target Month:", target_month)
+print("Reason:", reason)
+print("Daily Action:", daily_action)
+
+print("======================================")
+print("        KEEP WORKING TOWARDS IT!")
+print("======================================")
